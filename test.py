3cc8f5a08dba810bf84243830b6cde0f5a61f754
Panan911 <nocool@126.com>
2022-06-15 22:46:21 +0800

    20220615

--- test.py
+++
@@ -1,8 +0,0 @@
-def pt(sn):
-	print(sn)
-	return sn
-
-sn = 0
-while pt(sn) != 10 :
-	sn = sn + 1
-
--- test2.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-from PIL import Image
-# 读取图片
-im = Image.open("skill.png")
-# 识别文字
-string = pytesseract.image_to_string(im, lang="chi_sim").encode('gb18030').decode('big5')
-print(string)
